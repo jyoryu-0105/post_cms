a8cb95bbe5e614757efc202d05b51d902b511d7c
jyoryu-0105 <ryutaro.nakanoue@gmail.com>
2021-04-12 17:53:28 +0900

    テストコードの実装

--- app/models/order.rb
+++ app/models/order.rb
@@ -8,10 +8,10 @@ class Order < ApplicationRecord
     validates :third_keyword
     validates :fourth_keyword
     validates :fifth_keyword
-    validates :words, format: { with: /\d{5}/, message: 'を入力してください' }
-    validates :price, format: { with: /\d{5}/, message: 'を入力してください' }
+    validates :words, format: { with: /\A\d{3,5}\z/, message: 'を入力してください' }
+    validates :price, format: { with:/\A\d{3,5}\z/, message: 'を入力してください' }
   end
-  
+
   def self.import(file)
     CSV.foreach(file.path, headers: true, encoding: 'Shift_JIS:UTF-8') do |row|
       order = find_by(id: row['id']) || new
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_12_055509) do
+ActiveRecord::Schema.define(version: 2021_04_12_084259) do
 
   create_table "orders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "title", null: false
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 2021_04_12_055509) do
     t.integer "price", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -40,4 +42,5 @@ ActiveRecord::Schema.define(version: 2021_04_12_055509) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "orders", "users"
 end
