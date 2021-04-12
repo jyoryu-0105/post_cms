8b5239ddd876e27950118a30fe12f1281e87958b
jyoryu-0105 <ryutaro.nakanoue@gmail.com>
2021-04-12 15:20:42 +0900

    モデルの依存関係の記載とコントローラーの修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,15 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :checking_admin
+
+  def index
+  end
+
+  private
+
+  def checking_admin
+    if @user.admin_flg == false
+      redirect_to root_path
+    end
+  end 
 end
