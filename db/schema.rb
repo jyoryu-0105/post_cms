d0f6c6bf6745f06b9792679d06ca82458e7a05b0
jyoryu-0105 <ryutaro.nakanoue@gmail.com>
2021-04-12 18:33:45 +0900

    コントローラーとモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_12_084259) do
+ActiveRecord::Schema.define(version: 2021_04_12_093235) do
+
+  create_table "applies", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "order_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["order_id"], name: "index_applies_on_order_id"
+    t.index ["user_id"], name: "index_applies_on_user_id"
+  end
 
   create_table "orders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "title", null: false
@@ -42,5 +51,7 @@ ActiveRecord::Schema.define(version: 2021_04_12_084259) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "applies", "orders"
+  add_foreign_key "applies", "users"
   add_foreign_key "orders", "users"
 end
