9749cf39bf562d1e5b4f0457cdb675116989f6b1
jyoryu-0105 <ryutaro.nakanoue@gmail.com>
2021-04-16 18:25:43 +0900

    記事作成開始機能のプロトタイプ作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,25 @@ class PostsController < ApplicationController
   def index
     @applies = Apply.where(user_id: current_user.id)
   end
+
+  def new
+    @applies = Apply.where(user_id: current_user.id)
+  end
+
+  def create
+    @post = Post.new(post_params)
+    binding.pry
+    if @post.save
+      redirect_to applies_path, notice: '応募が完了しました。'
+    else
+      redirect_to new_apply_path, alert: '応募に失敗しました。もう一度お試しください'
+    end
+  end
+
+  private
+
+  def post_params
+    params.permit(:title, :user_id, :order_id).merge(title: params[:title], user_id: current_user.id, order_id: params[:order_id])
+  end
+
 end
