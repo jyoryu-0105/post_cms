d0136557d248382286a1f4782b92df4ddcc13362
jyoryu-0105 <ryutaro.nakanoue@gmail.com>
2021-04-28 00:48:47 +0900

    basic_authの記載

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :set_user
+  before_action :basic_auth
 
   def set_user
     @user = current_user
+  end
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'admin' && password == '2222'
+    end
   end
 end
