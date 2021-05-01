c50f113fa090d6036c47db98c2934d86ce997ce7
jyoryu-0105 <ryutaro.nakanoue@gmail.com>
2021-05-01 18:17:02 +0900

    admin側のPostモデルの修正

--- app/controllers/admins/posts_controller.rb
+++ app/controllers/admins/posts_controller.rb
@@ -1,6 +1,5 @@
 class Admins::PostsController < ApplicationController
-  before_action :already_created
-  before_action :set_post, only: [:show, :edit, :update, :deliver, :declined, :publish]
+  before_action :set_post, only: [:show, :declined, :publish]
 
   def index
     @applies = Apply.where(user_id: current_user.id)
@@ -10,52 +9,21 @@ class Admins::PostsController < ApplicationController
     @applies = Apply.where(user_id: current_user.id)
   end
 
-  def create
-    @post = Post.new(post_params)
-    if @post.save
-      redirect_to new_post_path, notice: '作成が開始できます。'
-    else
-      redirect_to new_post_path, alert: '記事作成に失敗しました。もう一度お試しください'
-    end
-  end
 
   def show
   end
 
-  def edit
-  end
-
-  def update
-      if @post.update(post_content_params)
-        redirect_to edit_post_path, notice: '正常に保存されました。'
-      else
-        redirect_to edit_post_path, alert: '更新に失敗しました。もう一度お試しください'
-      end
-  end
-
-  def draft
-    @posts = Post.where(user_id: current_user.id, post_status: 0)
-  end
-
-  def deliver
-    if @post.update_attribute(post_status: 1)
-      redirect_to draft_posts_path, notice: '正常に納品されました。'
-    else
-      redirect_to edit_post_path, alert: '納品に失敗しました。もう一度お試しください。'
-    end
-  end
-
   def declined
-    if @post.update_attribute(post_status: 0)
-      redirect_to delivered_posts_path, notice: '正常に差し戻されました。'
+    if @post.update(declined_params)
+      redirect_to delivered_admins_posts_path, notice: '正常に差し戻されました。'
     else
       render :show, alert: '差し戻しに失敗しました。もう一度お試しください。'
     end
   end
 
   def publish
-    if @post.update_attributes(post_status: 2, publish_date: Time.now)
-      redirect_to delivered_posts_path, notice: '正常に公開されました。'
+    if @post.update(publish_params)
+      redirect_to delivered_admins_posts_path, notice: '正常に公開されました。'
     else
       render :show, alert: '公開に失敗しました。もう一度お試しください。'
     end
@@ -79,11 +47,12 @@ class Admins::PostsController < ApplicationController
     params.permit(:title, :user_id, :order_id).merge(title: params[:title], user_id: current_user.id, order_id: params[:order_id])
   end
 
-  def post_content_params
-    params.require(:post).permit(:content)
+  def declined_params
+    params.permit(:post_status).merge(post_status: 0)
   end
 
-  def already_created
-    redirect_to new_post_path, alert: 'この記事はすでに作成が開始されています。' if Post.exists?(order_id: params[:order_id])
+  def publish_params
+    params.permit(:post_status, :publish_date).merge(post_status: 2, publish_date: Time.now)
   end
+
 end
