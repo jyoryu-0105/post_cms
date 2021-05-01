b796c307657b221c22a6dc9dddc12de594386308
jyoryu-0105 <ryutaro.nakanoue@gmail.com>
2021-05-01 19:31:33 +0900

    ユーザー側のダッシュボードの作成

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -1,5 +1,15 @@
 class Users::UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_draft
+
   def index
+    @exist = Order.where(user_id: current_user.id, created_at: Date.today.beginning_of_day..Date.today.end_of_day)
+    @exists = Order.where(user_id: current_user.id)
+  end
+
+  private
+
+  def set_draft
+    @posts = Post.where(user_id: current_user.id, post_status: 0)
   end
 end
