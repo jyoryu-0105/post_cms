40070867781457870a390d40e1382e59b52767dd
jyoryu-0105 <ryutaro.nakanoue@gmail.com>
2021-05-13 18:39:13 +0900

    adminのテストコード作成

--- spec/factories/admins.rb
+++ spec/factories/admins.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :admin do
-    
+    nickname              { Faker::Name.initials(number: 2) }
+    email                 { Faker::Internet.free_email }
+    password              { Faker::Internet.password(min_length: 6, mix_case: true) }
+    password_confirmation { password }
   end
 end
