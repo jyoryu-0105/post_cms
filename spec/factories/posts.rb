0ba498d8f3da992814bcebbd2f0b4859938af7c8
jyoryu-0105 <ryutaro.nakanoue@gmail.com>
2021-05-14 16:55:31 +0900

    postモデルのテストコードの作成

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :post do
-    
+    title              { Faker::Lorem.words(number: 1) }
+    post_status        { 1 }
+    user_id              { Faker::Number.number(digits: 1)}
+    order_id              { Faker::Number.number(digits: 1)}
   end
 end
