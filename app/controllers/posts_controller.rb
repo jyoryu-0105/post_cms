e706cc26b52343505f9040a617c3a131dda01bfe
jyoryu-0105 <ryutaro.nakanoue@gmail.com>
2021-05-17 16:15:19 +0900

    Postコントローラーの修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show]
+  before_action :set_post, only: [:show, :update]
 
   def index
     @posts = Post.where(post_status: 2)
@@ -8,10 +8,21 @@ class PostsController < ApplicationController
   def show
   end
 
+  def update
+    if @post.update(post_content_params)
+      redirect_to edit_admins_post_path, notice: '正常に保存されました。'
+    else
+      redirect_to edit_admins_post_path, alert: '更新に失敗しました。もう一度お試しください'
+    end
+  end
+
   private
 
   def set_post
     @post = Post.find(params[:id])
   end
 
+  def post_content_params
+    params.require(:post).permit(:content)
+  end
 end
