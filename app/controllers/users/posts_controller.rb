e47b4dbb381375af4d5305f5fa898b72d4651e26
jyoryu-0105 <ryutaro.nakanoue@gmail.com>
2021-05-17 17:40:33 +0900

    gemfile等の修正

--- app/controllers/users/posts_controller.rb
+++ app/controllers/users/posts_controller.rb
@@ -1,6 +1,6 @@
 class Users::PostsController < ApplicationController
   before_action :already_created
-  before_action :set_post, only: [:edit, :deliver]
+  before_action :set_post, only: [:edit, :deliver, :update]
 
   def index
     @applies = Apply.where(user_id: current_user.id)
@@ -16,6 +16,14 @@ class Users::PostsController < ApplicationController
       redirect_to draft_users_posts_path, notice: '作成が開始できます。'
     else
       redirect_to new_users_post_path, alert: '記事作成に失敗しました。もう一度お試しください'
+    end
+  end
+
+  def update
+     if @post.update(post_content_params)
+      redirect_to edit_admins_post_path, notice: '正常に保存されました。'
+    else
+      redirect_to edit_admins_post_path, alert: '更新に失敗しました。もう一度お試しください'
     end
   end
 
@@ -55,5 +63,9 @@ class Users::PostsController < ApplicationController
 
   def already_created
     redirect_to new_users_post_path, alert: 'この記事はすでに作成が開始されています。' if Post.exists?(order_id: params[:order_id])
+  end
+
+    def post_content_params
+    params.require(:post).permit(:content)
   end
 end
